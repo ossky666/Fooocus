e948e289a615802765bc43f0d03b184feb78faea
ossky666 <141194947+ossky666@users.noreply.github.com>
2023-09-19 16:57:57 +0800

    Add files via upload

---
+++ modules/default_pipeline.py
@@ -0,0 +1,259 @@
+import modules.core as core
+import os
+import torch
+import modules.path
+import modules.virtual_memory as virtual_memory
+
+from comfy.model_base import SDXL, SDXLRefiner
+from modules.patch import cfg_patched, patched_model_function
+from modules.expansion import FooocusExpansion
+
+
+xl_base: core.StableDiffusionModel = None
+xl_base_hash = ''
+
+xl_refiner: core.StableDiffusionModel = None
+xl_refiner_hash = ''
+
+xl_base_patched: core.StableDiffusionModel = None
+xl_base_patched_hash = ''
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def refresh_base_model(name):
+    global xl_base, xl_base_hash, xl_base_patched, xl_base_patched_hash
+
+    filename = os.path.abspath(os.path.realpath(os.path.join(modules.path.modelfile_path, name)))
+    model_hash = filename
+
+    if xl_base_hash == model_hash:
+        return
+
+    if xl_base is not None:
+        xl_base.to_meta()
+        xl_base = None
+
+    xl_base = core.load_model(filename)
+    if not isinstance(xl_base.unet.model, SDXL):
+        print('Model not supported. Fooocus only support SDXL model as the base model.')
+        xl_base = None
+        xl_base_hash = ''
+        refresh_base_model(modules.path.default_base_model_name)
+        xl_base_hash = model_hash
+        xl_base_patched = xl_base
+        xl_base_patched_hash = ''
+        return
+
+    xl_base_hash = model_hash
+    xl_base_patched = xl_base
+    xl_base_patched_hash = ''
+    print(f'Base model loaded: {model_hash}')
+    return
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def refresh_refiner_model(name):
+    global xl_refiner, xl_refiner_hash
+
+    filename = os.path.abspath(os.path.realpath(os.path.join(modules.path.modelfile_path, name)))
+    model_hash = filename
+
+    if xl_refiner_hash == model_hash:
+        return
+
+    if name == 'None':
+        xl_refiner = None
+        xl_refiner_hash = ''
+        print(f'Refiner unloaded.')
+        return
+
+    if xl_refiner is not None:
+        xl_refiner.to_meta()
+        xl_refiner = None
+
+    xl_refiner = core.load_model(filename)
+    if not isinstance(xl_refiner.unet.model, SDXLRefiner):
+        print('Model not supported. Fooocus only support SDXL refiner as the refiner.')
+        xl_refiner = None
+        xl_refiner_hash = ''
+        print(f'Refiner unloaded.')
+        return
+
+    xl_refiner_hash = model_hash
+    print(f'Refiner model loaded: {model_hash}')
+
+    xl_refiner.vae.first_stage_model.to('meta')
+    xl_refiner.vae = None
+    return
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def refresh_loras(loras):
+    global xl_base, xl_base_patched, xl_base_patched_hash
+    if xl_base_patched_hash == str(loras):
+        return
+
+    model = xl_base
+    for name, weight in loras:
+        if name == 'None':
+            continue
+
+        filename = os.path.join(modules.path.lorafile_path, name)
+        model = core.load_lora(model, filename, strength_model=weight, strength_clip=weight)
+    xl_base_patched = model
+    xl_base_patched_hash = str(loras)
+    print(f'LoRAs loaded: {xl_base_patched_hash}')
+
+    return
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def clip_encode_single(clip, text, verbose=False):
+    cached = clip.fcs_cond_cache.get(text, None)
+    if cached is not None:
+        if verbose:
+            print(f'[CLIP Cached] {text}')
+        return cached
+    tokens = clip.tokenize(text)
+    result = clip.encode_from_tokens(tokens, return_pooled=True)
+    clip.fcs_cond_cache[text] = result
+    if verbose:
+        print(f'[CLIP Encoded] {text}')
+    return result
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def clip_encode(sd, texts, pool_top_k=1):
+    if sd is None:
+        return None
+    if sd.clip is None:
+        return None
+    if not isinstance(texts, list):
+        return None
+    if len(texts) == 0:
+        return None
+
+    clip = sd.clip
+    cond_list = []
+    pooled_acc = 0
+
+    for i, text in enumerate(texts):
+        cond, pooled = clip_encode_single(clip, text)
+        cond_list.append(cond)
+        if i < pool_top_k:
+            pooled_acc += pooled
+
+    return [[torch.cat(cond_list, dim=1), {"pooled_output": pooled_acc}]]
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def clear_sd_cond_cache(sd):
+    if sd is None:
+        return None
+    if sd.clip is None:
+        return None
+    sd.clip.fcs_cond_cache = {}
+    return
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def clear_all_caches():
+    clear_sd_cond_cache(xl_base_patched)
+    clear_sd_cond_cache(xl_refiner)
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def refresh_everything(refiner_model_name, base_model_name, loras):
+    refresh_refiner_model(refiner_model_name)
+    if xl_refiner is not None:
+        virtual_memory.try_move_to_virtual_memory(xl_refiner.unet.model)
+        virtual_memory.try_move_to_virtual_memory(xl_refiner.clip.cond_stage_model)
+
+    refresh_base_model(base_model_name)
+    virtual_memory.load_from_virtual_memory(xl_base.unet.model)
+
+    refresh_loras(loras)
+    clear_all_caches()
+    return
+
+
+refresh_everything(
+    refiner_model_name=modules.path.default_refiner_model_name,
+    base_model_name=modules.path.default_base_model_name,
+    loras=[(modules.path.default_lora_name, 0.5), ('None', 0.5), ('None', 0.5), ('None', 0.5), ('None', 0.5)]
+)
+
+expansion = FooocusExpansion()
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def patch_all_models():
+    assert xl_base is not None
+    assert xl_base_patched is not None
+
+    xl_base.unet.model_options['sampler_cfg_function'] = cfg_patched
+    xl_base.unet.model_options['model_function_wrapper'] = patched_model_function
+
+    xl_base_patched.unet.model_options['sampler_cfg_function'] = cfg_patched
+    xl_base_patched.unet.model_options['model_function_wrapper'] = patched_model_function
+
+    if xl_refiner is not None:
+        xl_refiner.unet.model_options['sampler_cfg_function'] = cfg_patched
+        xl_refiner.unet.model_options['model_function_wrapper'] = patched_model_function
+
+    return
+
+
+@torch.no_grad()
+@torch.inference_mode()
+def process_diffusion(positive_cond, negative_cond, steps, switch, width, height, image_seed, callback, latent=None, denoise=1.0, tiled=False):
+    patch_all_models()
+
+    if xl_refiner is not None:
+        virtual_memory.try_move_to_virtual_memory(xl_refiner.unet.model)
+    virtual_memory.load_from_virtual_memory(xl_base.unet.model)
+
+    if latent is None:
+        empty_latent = core.generate_empty_latent(width=width, height=height, batch_size=1)
+    else:
+        empty_latent = latent
+
+    if xl_refiner is not None:
+        sampled_latent = core.ksampler_with_refiner(
+            model=xl_base_patched.unet,
+            positive=positive_cond[0],
+            negative=negative_cond[0],
+            refiner=xl_refiner.unet,
+            refiner_positive=positive_cond[1],
+            refiner_negative=negative_cond[1],
+            refiner_switch_step=switch,
+            latent=empty_latent,
+            steps=steps, start_step=0, last_step=steps, disable_noise=False, force_full_denoise=True,
+            seed=image_seed,
+            denoise=denoise,
+            callback_function=callback
+        )
+    else:
+        sampled_latent = core.ksampler(
+            model=xl_base_patched.unet,
+            positive=positive_cond[0],
+            negative=negative_cond[0],
+            latent=empty_latent,
+            steps=steps, start_step=0, last_step=steps, disable_noise=False, force_full_denoise=True,
+            seed=image_seed,
+            denoise=denoise,
+            callback_function=callback
+        )
+
+    decoded_latent = core.decode_vae(vae=xl_base_patched.vae, latent_image=sampled_latent, tiled=tiled)
+    images = core.pytorch_to_numpy(decoded_latent)
+    return images
---
+++ modules/patch.py
@@ -0,0 +1,208 @@
+import torch
+import comfy.model_base
+import comfy.ldm.modules.diffusionmodules.openaimodel
+import comfy.samplers
+import comfy.k_diffusion.external
+import comfy.model_management
+import modules.anisotropic as anisotropic
+import comfy.ldm.modules.attention
+import comfy.k_diffusion.sampling
+import comfy.sd1_clip
+import modules.inpaint_worker as inpaint_worker
+
+from comfy.k_diffusion import utils
+from comfy.k_diffusion.sampling import BrownianTreeNoiseSampler, trange
+
+
+sharpness = 2.0
+negative_adm = True
+
+cfg_x0 = 0.0
+cfg_s = 1.0
+cfg_cin = 1.0
+
+
+def cfg_patched(args):
+    global cfg_x0, cfg_s
+    positive_eps = args['cond'].clone()
+    positive_x0 = args['cond'] * cfg_s + cfg_x0
+    uncond = args['uncond'] * cfg_s + cfg_x0
+    cond_scale = args['cond_scale']
+    t = args['timestep']
+
+    alpha = 1.0 - (t / 999.0)[:, None, None, None].clone()
+    alpha *= 0.001 * sharpness
+
+    eps_degraded = anisotropic.adaptive_anisotropic_filter(x=positive_eps, g=positive_x0)
+    eps_degraded_weighted = eps_degraded * alpha + positive_eps * (1.0 - alpha)
+
+    cond = eps_degraded_weighted * cfg_s + cfg_x0
+
+    return uncond + (cond - uncond) * cond_scale
+
+
+def patched_discrete_eps_ddpm_denoiser_forward(self, input, sigma, **kwargs):
+    global cfg_x0, cfg_s, cfg_cin
+    c_out, c_in = [utils.append_dims(x, input.ndim) for x in self.get_scalings(sigma)]
+    cfg_x0 = input
+    cfg_s = c_out
+    cfg_cin = c_in
+    return self.get_eps(input * c_in, self.sigma_to_t(sigma), **kwargs)
+
+
+def patched_model_function(func, args):
+    global cfg_cin
+    x = args['input']
+    t = args['timestep']
+    c = args['c']
+    is_uncond = torch.tensor(args['cond_or_uncond'])[:, None, None, None].to(x) * 5e-3
+    if inpaint_worker.current_task is not None:
+        p = inpaint_worker.current_task.uc_guidance * cfg_cin
+        x = p * is_uncond + x * (1 - is_uncond ** 2.0) ** 0.5
+    return func(x, t, **c)
+
+
+def sdxl_encode_adm_patched(self, **kwargs):
+    global negative_adm
+
+    clip_pooled = kwargs["pooled_output"]
+    width = kwargs.get("width", 768)
+    height = kwargs.get("height", 768)
+    crop_w = kwargs.get("crop_w", 0)
+    crop_h = kwargs.get("crop_h", 0)
+    target_width = kwargs.get("target_width", width)
+    target_height = kwargs.get("target_height", height)
+
+    if negative_adm:
+        if kwargs.get("prompt_type", "") == "negative":
+            width *= 0.8
+            height *= 0.8
+        elif kwargs.get("prompt_type", "") == "positive":
+            width *= 1.5
+            height *= 1.5
+
+    out = []
+    out.append(self.embedder(torch.Tensor([height])))
+    out.append(self.embedder(torch.Tensor([width])))
+    out.append(self.embedder(torch.Tensor([crop_h])))
+    out.append(self.embedder(torch.Tensor([crop_w])))
+    out.append(self.embedder(torch.Tensor([target_height])))
+    out.append(self.embedder(torch.Tensor([target_width])))
+    flat = torch.flatten(torch.cat(out))[None, ]
+    return torch.cat((clip_pooled.to(flat.device), flat), dim=1)
+
+
+def text_encoder_device_patched():
+    # Fooocus's style system uses text encoder much more times than comfy so this makes things much faster.
+    return comfy.model_management.get_torch_device()
+
+
+def encode_token_weights_patched_with_a1111_method(self, token_weight_pairs):
+    to_encode = list(self.empty_tokens)
+    for x in token_weight_pairs:
+        tokens = list(map(lambda a: a[0], x))
+        to_encode.append(tokens)
+
+    out, pooled = self.encode(to_encode)
+
+    z_empty = out[0:1]
+    if pooled.shape[0] > 1:
+        first_pooled = pooled[1:2]
+    else:
+        first_pooled = pooled[0:1]
+
+    output = []
+    for k in range(1, out.shape[0]):
+        z = out[k:k + 1]
+        original_mean = z.mean()
+
+        for i in range(len(z)):
+            for j in range(len(z[i])):
+                weight = token_weight_pairs[k - 1][j][1]
+                z[i][j] = (z[i][j] - z_empty[0][j]) * weight + z_empty[0][j]
+
+        new_mean = z.mean()
+        z = z * (original_mean / new_mean)
+        output.append(z)
+
+    if len(output) == 0:
+        return z_empty.cpu(), first_pooled.cpu()
+    return torch.cat(output, dim=-2).cpu(), first_pooled.cpu()
+
+
+@torch.no_grad()
+def sample_dpmpp_fooocus_2m_sde_inpaint_seamless(model, x, sigmas, extra_args=None, callback=None, disable=None, eta=1., s_noise=1., noise_sampler=None, **kwargs):
+    sigma_min, sigma_max = sigmas[sigmas > 0].min(), sigmas.max()
+    noise_sampler = BrownianTreeNoiseSampler(x, sigma_min, sigma_max, seed=extra_args.get("seed", None), cpu=False) if noise_sampler is None else noise_sampler
+
+    seed = extra_args.get("seed", None)
+    assert isinstance(seed, int)
+
+    energy_generator = torch.Generator(device='cpu')
+    energy_generator.manual_seed(seed + 1)  # avoid bad results by using different seeds.
+
+    def get_energy():
+        return torch.randn(x.size(), dtype=x.dtype, generator=energy_generator, device="cpu").to(x)
+
+    sigma_min, sigma_max = sigmas[sigmas > 0].min(), sigmas.max()
+    noise_sampler = BrownianTreeNoiseSampler(x, sigma_min, sigma_max, seed=seed, cpu=True) if noise_sampler is None else noise_sampler
+    extra_args = {} if extra_args is None else extra_args
+    s_in = x.new_ones([x.shape[0]])
+
+    old_denoised, h_last, h = None, None, None
+
+    latent_processor = model.inner_model.inner_model.inner_model.process_latent_in
+    inpaint_latent = None
+    inpaint_mask = None
+
+    if inpaint_worker.current_task is not None:
+        inpaint_latent = latent_processor(inpaint_worker.current_task.latent).to(x)
+        inpaint_mask = inpaint_worker.current_task.latent_mask.to(x)
+
+    def blend_latent(a, b, w):
+        return a * w + b * (1 - w)
+
+    for i in trange(len(sigmas) - 1, disable=disable):
+        if inpaint_latent is None:
+            denoised = model(x, sigmas[i] * s_in, **extra_args)
+        else:
+            inpaint_worker.current_task.uc_guidance = x.detach().clone()
+            energy = get_energy() * sigmas[i] + inpaint_latent
+            x_prime = blend_latent(x, energy, inpaint_mask)
+            denoised = model(x_prime, sigmas[i] * s_in, **extra_args)
+            denoised = blend_latent(denoised, inpaint_latent, inpaint_mask)
+        if callback is not None:
+            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigmas[i], 'denoised': denoised})
+        if sigmas[i + 1] == 0:
+            x = denoised
+        else:
+            t, s = -sigmas[i].log(), -sigmas[i + 1].log()
+            h = s - t
+            eta_h = eta * h
+
+            x = sigmas[i + 1] / sigmas[i] * (-eta_h).exp() * x + (-h - eta_h).expm1().neg() * denoised
+            if old_denoised is not None:
+                r = h_last / h
+                x = x + 0.5 * (-h - eta_h).expm1().neg() * (1 / r) * (denoised - old_denoised)
+
+            x = x + noise_sampler(sigmas[i], sigmas[i + 1]) * sigmas[i + 1] * (
+                        -2 * eta_h).expm1().neg().sqrt() * s_noise
+
+        old_denoised = denoised
+        h_last = h
+
+    return x
+
+
+def patch_all():
+    comfy.ldm.modules.attention.print = lambda x: None
+    comfy.k_diffusion.sampling.sample_dpmpp_fooocus_2m_sde_inpaint_seamless = sample_dpmpp_fooocus_2m_sde_inpaint_seamless
+
+    comfy.model_management.text_encoder_device = text_encoder_device_patched
+    print(f'Fooocus Text Processing Pipelines are retargeted to {str(comfy.model_management.text_encoder_device())}')
+
+    comfy.k_diffusion.external.DiscreteEpsDDPMDenoiser.forward = patched_discrete_eps_ddpm_denoiser_forward
+    comfy.model_base.SDXL.encode_adm = sdxl_encode_adm_patched
+
+    comfy.sd1_clip.ClipTokenWeightEncoder.encode_token_weights = encode_token_weights_patched_with_a1111_method
+    return
